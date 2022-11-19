b9600a0960cfcdb60466e74883d03e7ee007f744
spiritman90 <phemmyadey@gmail.com>
2022-11-19 16:53:52 +0100

    Onsubmit

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 import InputField from "./components/InputField";
+import { Todo } from "./model";
 
 const App: React.FC = () => {
+  const [todo, setTodo] = useState<string>("");
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  // console.log(todo);
+
+  const handleAdd = () => {};
   return (
     <div className="App">
       <span className="heading">Task Manager</span>
-      <InputField />
+      <InputField todo={todo} setTodo={setTodo} handleAdd={handleAdd} />
     </div>
   );
 };
